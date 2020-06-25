963cab5ce715a5d68e027ab48808b62d3665f003
Clustern <homerun372@gmail.com>
2020-06-26 00:42:12 +0900

    변수 추가

--- js/initializer.js
+++ js/initializer.js
@@ -1,7 +1,10 @@
 //변수 선언, 초기값 입력, 페이지 로드 시 명령 호출 관련
 
-var user_info = new Array();    //유저 정보 저장
-var post = new Array();         //포스터 정보 저장
+var user_info = new Array();    //유저 정보 저장    [아이디, 비밀번호, 닉네임, 이메일]
+var movie_info = new Array();   //영화 정보 저장    [제목, 장르, 개봉연도]
+var post = new Array();         //포스터 정보 저장  [제목, 영화제목, 게시자, 게시일, 포스트내용]
+var is_login = false;           //로그인 상태
+
 
 //관리자 계정 추가
 user_info.push(["master", "master", "개발자", "master@gmail.com", 0]);
