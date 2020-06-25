5d7f335e0776b72b85737af39ea52e5b629474ba
Clustern <homerun372@gmail.com>
2020-06-26 00:31:56 +0900

    displayer 브랜치에 맞게 수정

--- initializer.js
+++ initializer.js
@@ -1,5 +1,13 @@
+//변수 선언, 초기값 입력, 페이지 로드 시 명령 호출 관련
+
 var user_info = new Array();    //유저 정보 저장
 var post = new Array();         //포스터 정보 저장
 
 //관리자 계정 추가
-user_info[0] = ["master", "master", "개발자", "master@gmail.com", 0];
+user_info.push(["master", "master", "개발자", "master@gmail.com", 0]);
+
+//더미 포스트 데이터
+post.push(["제목", "영화제목", "닉네임", "날짜", "포스트내용"]);
+
+//로드 후 초기 페이지
+list_displayer();
